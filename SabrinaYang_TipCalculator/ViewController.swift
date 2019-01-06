086321a353eb0760c90d7bebc5cee8cea5fdad04
sabrina <sabrinayanggg@gmail.com>
2019-01-06 15:21:31 -0800

    Initial Commit

---
+++ SabrinaYang_TipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SabrinaYang_TipCalculator
+//
+//  Created by sabrina on 1/6/19.
+//  Copyright © 2019 sabrina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SabrinaYang_TipCalculatorTests/SabrinaYang_TipCalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  SabrinaYang_TipCalculatorTests.swift
+//  SabrinaYang_TipCalculatorTests
+//
+//  Created by sabrina on 1/6/19.
+//  Copyright © 2019 sabrina. All rights reserved.
+//
+
+import XCTest
+@testable import SabrinaYang_TipCalculator
+
+class SabrinaYang_TipCalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
